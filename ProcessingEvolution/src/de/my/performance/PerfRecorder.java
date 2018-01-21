3ab9a06dd445f407fdde06e7fe41fd8fef092b5a
Oliver Meyer <Oliver Meyer@Krull>
2018-01-21 10:02:22 +0100

    Initial version of performance tuning of Evolution project
    Evolution3WEB is the java export of the "original" processing sketch,
    Evolution Simulator [FIXED] by Cary Huang,  moved into a java package. 
    Evolution is my go at creating a java version and will soon be deleted.
    Simulator is still dead code.

---
+++ ProcessingEvolution/src/de/my/performance/PerfRecorder.java
@@ -0,0 +1,130 @@
+package de.my.performance;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static de.my.performance.PerfRecorder.InternalState.*;
+
+/**
+ * Stop-Watch like measure. It may keep one current time.
+ * 
+ * @author Oliver Meyer
+ *
+ */
+public class PerfRecorder {
+	protected enum InternalState {
+		Running, // We are currently doing a measure (started, not yet stopped)
+		NotStarted, // Not yet started, no pending write
+		Stopped, // We have been stopped and can eventually write the timing.
+	}
+
+	private PrintStream perfLog;
+	private long endTime;
+	private long startTime;
+	private InternalState state = NotStarted;
+	private String label;
+
+	static private PerfRecorder singleton = new PerfRecorder();
+
+	public static PerfRecorder instance() {
+		return singleton;
+	}
+
+	public PerfRecorder() {
+		final SimpleDateFormat form = new SimpleDateFormat("yyMMdd-HHmmss");
+		final Date now = new Date();
+		init("It took: ", "perf_" + form.format(now));
+	}
+
+	public PerfRecorder(String label, String file) {
+		init(label, file);
+	}
+
+	/**
+	 * Updates the label to be used in the next recording.
+	 * 
+	 * @param label
+	 *            The label to use for all subsequent recordings.
+	 */
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	/**
+	 * Start a new timing. Will restart the timing if already running.
+	 */
+	public void startTiming() {
+		beginMeasure();
+	}
+
+
+	/**
+	 * Stops, and records the measure.
+	 * 
+	 * Record the measure into the performance file using the current label.
+	 * Ensures stop, but will only record, if we are measuring.
+	 */
+	public void stopTiming() {
+		switch (state) {
+		case NotStarted:
+		case Stopped:
+			break;
+		case Running:
+			endMeasure();
+			writeTiming();
+		}
+		state = NotStarted;
+	}
+
+	/**
+	 * Stops, records, and starts the timing.
+	 */
+	public void recordIteration() {
+		stopTiming();
+		startTiming();
+	}
+
+	/**
+	 * Record the current timing, but continue measuring.
+	 */
+	public void dumpTiming() {
+		switch (state) {
+		case NotStarted:
+			break;
+		case Stopped:
+		case Running:
+			writeTiming();
+			break;
+		}
+	}
+
+	/**
+	 * Writes the current label and measure to the log. Stateless.
+	 */
+	private void writeTiming() {
+		perfLog.println(label + (endTime - startTime));
+		perfLog.flush();
+	}
+
+	private void beginMeasure() {
+		startTime = System.currentTimeMillis();
+		state = Running;
+	}
+	
+	private void endMeasure() {
+		endTime = System.currentTimeMillis();
+		state = Stopped;
+	}
+
+	protected void init(String label, String filename) {
+		this.label = label;
+		try {
+			perfLog = new PrintStream(filename, "ASCII");
+		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ ProcessingEvolution/src/de/my/tryouts/testConversion.java
@@ -0,0 +1,12 @@
+package de.my.tryouts;
+public class testConversion {
+
+	public static void main(String[] args) {
+		float f = 0.9f;
+		System.out.println("0.9 als int="+(int)f);
+		System.out.println("0.9 als String="+f);
+		float d = 10/3f;
+		System.out.println("10-3="+d);
+	}
+
+}
