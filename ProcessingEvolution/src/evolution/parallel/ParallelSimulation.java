4edc2aac81ac05bf6e0dabe10c2c4939153ed467
Oliver Meyer <Oliver Meyer@Krull>
2018-02-16 20:35:54 +0100

    Removed unnecessary import

--- ProcessingEvolution/src/evolution/parallel/ParallelSimulation.java
+++ ProcessingEvolution/src/evolution/parallel/ParallelSimulation.java
@@ -1,7 +1,6 @@
 package evolution.parallel;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
